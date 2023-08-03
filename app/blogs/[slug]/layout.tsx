05aa6ef4574f9fb68d317757d3529b5fa3646979
Sathish <sathish.uthayasuriyan@coindcx.com>
2023-08-03 21:12:55 +0530

    type fix

--- app/blogs/[slug]/layout.tsx
+++ app/blogs/[slug]/layout.tsx
@@ -1,4 +1,6 @@
-export default function BlogLayout({children}) {
+export default function BlogLayout({children}: {
+  children: React.ReactNode
+}) {
   return <>
     <h1>Blog layout</h1>
     {children}
--- app/provider/theme-providerr.tsx
+++ app/provider/theme-providerr.tsx
@@ -4,6 +4,8 @@ import { createContext } from 'react'
  
 export const ThemeContext = createContext({})
  
-export default function ThemeProvider({ children }) {
+export default function ThemeProvider({ children }: {
+  children: React.ReactNode
+}) {
   return <ThemeContext.Provider value="dark">{children}</ThemeContext.Provider>
 }
