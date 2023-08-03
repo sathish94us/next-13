18548922c6151999c1c503372ffcefcccc196e9c
Sathish <sathish.uthayasuriyan@coindcx.com>
2023-08-03 21:30:54 +0530

    Added metadata

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,7 +1,9 @@
+import { Metadata } from "next";
+
 // Return a list of `params` to populate the [slug] dynamic segment
 export function generateStaticParams() {
     console.log('generateStaticParams Blog Slug');
-    return [1, 2, 3, 4, 5].map(number => ({slug: number + ''}));
+    return [1, 2, 3, 4, 5].map(number => ({ slug: number + '' }));
 }
 
 async function getData(value: string): Promise<string> {
@@ -12,7 +14,14 @@ async function getData(value: string): Promise<string> {
     })
 }
 
-export default async function BlogSlug({params}: {params: { slug: string }}) {
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+    return {
+        title: 'Blog Test Dynamic ' + params.slug,
+        description: 'Blog Test Dynamic Description : ' + params.slug
+    }
+}
+
+export default async function BlogSlug({ params }: { params: { slug: string } }) {
     const slug = params.slug;
     console.log('slug: ', slug);
     const data = await getData(slug);
