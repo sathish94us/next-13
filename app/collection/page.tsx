517b1208d56fcc62b34dad7e0764f8caedc08eba
Sathish <sathish.uthayasuriyan@coindcx.com>
2023-08-12 22:52:07 +0530

    time rsc

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -1,4 +1,4 @@
 export default function Collection() {
   console.log('Collection')
-  return <h1>Blog</h1>;
+  return <h1>Collection</h1>;
 }
---
+++ app/time/page.tsx
@@ -0,0 +1,5 @@
+export default function Time() {
+    console.log('Time')
+    return <h1>Time: {Date.now()}</h1>;
+  }
+  
