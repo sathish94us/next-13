f692f87c16a473da7ff03e1951f4417a90132d16
Sathish <sathish.uthayasuriyan@coindcx.com>
2023-08-17 20:22:10 +0530

    type fix

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 const useStore = create((set) => ({
  counter: 10,
- increment: () => set((state) => ({ counter: state.counter + 1 })),
- decrement: () => set((state) => ({ counter: state.counter - 1 })),
+ increment: () => set((state: any) => ({ counter: state.counter + 1 })),
+ decrement: () => set((state: any) => ({ counter: state.counter - 1 })),
  }));
 export default useStore;
