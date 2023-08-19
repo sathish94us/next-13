fdd100f094e3a711898378acdfe9f7378f719e08
Sathish <sathish.uthayasuriyan@coindcx.com>
2023-08-19 14:02:29 +0530

    added styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import Links from "./components/links";
+import styles from './styles.module.css'
 
 export default function Home() {
   console.log('Home')
-  return <>
+  return <div className={styles.container}>
     <h1>Home</h1>
     <Links />
-  </>
+  </div>
 }
